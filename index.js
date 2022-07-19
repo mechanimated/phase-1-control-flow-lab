d1b4dbcd2e0bc81ba85d6ed35793fe4a1a0ab025
Liz Boika <mdaicdxl@gmail.com>
2022-07-18 23:11:39 -0400

    commit tree bd8ce69c8f5393fb98c9a9b25273daf663819567

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length){
+ if(length <= 400) {
+  return 'This one is on me!';
+ }
+  else if(length <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if(length <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else(length > 2500);
+    return 'No can do.';
+  }
+ 
+
+function ternaryCheckCity(city){
+return ((city === 'NYC') ? "Ok, sounds good." : "No go.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
